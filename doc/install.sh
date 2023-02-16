0775bb9b4b56bf61490addefaebfedddf44b54cf
Salvydas Lukosius <sall@w-ss.io>
2023-02-16 07:50:16 +0000

    [maintenance]: fix minor lint issues

--- doc/install.sh
+++ doc/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if ! type git 2>/dev/null 1>&2; then
-    echo "Please install GIT first"
-    echo "Exiting"
-    exit 1
+  echo "Please install GIT first"
+  echo "Exiting"
+  exit 1
 fi
 
 #
@@ -11,20 +11,20 @@ fi
 #
 
 if ! test -d "$HOME/.config"; then
-    mkdir "$HOME/.config"
+  mkdir "$HOME/.config"
 fi
 
 if ! test -d "$HOME/.config/znt"; then
-    mkdir "$HOME/.config/znt"
+  mkdir "$HOME/.config/znt"
 fi
 
 echo ">>> Downloading zsh-navigation-tools to ~/.config/znt"
 if test -d ~/.config/znt/zsh-navigation-tools; then
-    cd ~/.config/znt/zsh-navigation-tools || exit
-    git pull origin main
+  cd ~/.config/znt/zsh-navigation-tools || exit
+  git pull origin main
 else
-    cd ~/.config/znt || exit
-    git clone https://github.com/z-shell/zsh-navigation-tools.git zsh-navigation-tools
+  cd ~/.config/znt || exit
+  git clone https://github.com/z-shell/zsh-navigation-tools.git zsh-navigation-tools
 fi
 echo ">>> Done"
 
@@ -39,9 +39,9 @@ cd ~/.config/znt || exit
 set n-aliases.conf n-env.conf n-history.conf n-list.conf n-panelize.conf n-cd.conf n-functions.conf n-kill.conf n-options.conf
 
 for i; do
-    if ! test -f "$i"; then
-        cp -v zsh-navigation-tools/.config/znt/"$i" .
-    fi
+  if ! test -f "$i"; then
+    cp -v zsh-navigation-tools/.config/znt/"$i" .
+  fi
 done
 
 echo ">>> Done"
@@ -52,19 +52,22 @@ echo ">>> Done"
 
 echo ">>> Updating .zshrc"
 if ! grep zsh-navigation-tools ~/.zshrc >/dev/null 2>&1; then
-    echo >>~/.zshrc
-    echo "### ZNT's installer added snippet ###" >>~/.zshrc
-    echo "fpath=( \"\$fpath[@]\" \"\$HOME/.config/znt/zsh-navigation-tools\" )" >>~/.zshrc
-    echo "autoload n-aliases n-cd n-env n-functions n-history n-kill n-list n-list-draw n-list-input n-options n-panelize n-help" >>~/.zshrc
-    echo "autoload znt-usetty-wrapper znt-history-widget znt-cd-widget znt-kill-widget" >>~/.zshrc
-    echo "alias naliases=n-aliases ncd=n-cd nenv=n-env nfunctions=n-functions nhistory=n-history" >>~/.zshrc
-    echo "alias nkill=n-kill noptions=n-options npanelize=n-panelize nhelp=n-help" >>~/.zshrc
-    echo "zle -N znt-history-widget" >>~/.zshrc
-    echo "bindkey '^R' znt-history-widget" >>~/.zshrc
-    echo "setopt AUTO_PUSHD HIST_IGNORE_DUPS PUSHD_IGNORE_DUPS" >>~/.zshrc
-    echo "zstyle ':completion::complete:n-kill::bits' matcher 'r:|=** l:|=*'" >>~/.zshrc
-    echo "### END ###" >>~/.zshrc
-    echo ">>> Done"
+  cat <<'EOF' >>~/.zshrc
+
+  ### ZNT's installer added snippet ###
+  fpath=( "$fpath[@]" "$HOME/.config/znt/zsh-navigation-tools" )
+  autoload n-aliases n-cd n-env n-functions n-history n-kill n-list n-list-draw n-list-input n-options n-panelize n-help
+  autoload znt-usetty-wrapper znt-history-widget znt-cd-widget znt-kill-widget
+  alias naliases=n-aliases ncd=n-cd nenv=n-env nfunctions=n-functions nhistory=n-history
+  alias nkill=n-kill noptions=n-options npanelize=n-panelize nhelp=n-help
+  zle -N znt-history-widget
+  bindkey '^R' znt-history-widget
+  setopt AUTO_PUSHD HIST_IGNORE_DUPS PUSHD_IGNORE_DUPS
+  zstyle ':completion::complete:n-kill::bits' matcher 'r:|=** l:|=*'
+  ### END ###
+
+EOF
+
 else
-    echo ">>> .zshrc already updated, not making changes"
+  echo ">>> .zshrc already updated, not making changes"
 fi
--- zsh-navigation-tools.plugin.zsh
+++ zsh-navigation-tools.plugin.zsh
@@ -66,6 +66,7 @@ unset __ZNT_CONFIG_FILES
 
 autoload n-aliases n-cd n-env n-functions n-history n-kill n-list n-list-draw n-list-input n-options n-panelize n-help
 autoload znt-usetty-wrapper znt-history-widget znt-cd-widget znt-kill-widget
+
 alias naliases=n-aliases ncd=n-cd nenv=n-env nfunctions=n-functions nhistory=n-history
 alias nkill=n-kill noptions=n-options npanelize=n-panelize nhelp=n-help
 
@@ -73,4 +74,3 @@ zle -N znt-history-widget
 bindkey '^R' znt-history-widget
 setopt AUTO_PUSHD HIST_IGNORE_DUPS PUSHD_IGNORE_DUPS
 zstyle ':completion::complete:n-kill::bits' matcher 'r:|=** l:|=*'
-
